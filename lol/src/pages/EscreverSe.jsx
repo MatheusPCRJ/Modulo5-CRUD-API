d48adad5220f347d597ef93f5c63d3fec8122bba
ricardo moraes de souza <ricardo.moraesd2@gmail.com>
2023-02-12 23:54:50 -0300

    parte do cadastro, mesmo que incompleto resolvi comitar pois meu pc n está salvando diariamente as alterações.

--- lol/src/pages/EscreverSe.jsx
+++ lol/src/pages/EscreverSe.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Outlet, Link } from "react-router-dom";
 import '../styles/EscreverSe.css'
-
+import ashe from "../image/ashe.png"
+import ashee from "../image/ashee.png"
 const EscreverSe = () => {
   return (
-    <div>
+    <div className='Img-Inscriv' style={{backgroundImage:`url(${ashe})`}}>
 
 <li>  <Link to="/">LandingPage</Link></li>
+<div className='inscriv-main'>
 
+<div className='cadastro'>
+    <h2>cadastre-se</h2>
+  <input id='Name' name='Name' placeholder='Name' type="text"  />
+  
+</div>
+<section className='Img-Inscriv2'>
+    <img src={ashee} alt=""  className='foto'/>
+</section>
+</div>
     </div>
   )
 }
