4b1f1a20c520dd2c84fc0a7df163e6ca2585f3f9
Programadores Cariocas 2022.1 <3968082022.1@senacrjedu.local>
2023-02-15 10:27:58 -0300

    Minha parte que não consegui colocar ontem, Diego

--- lol/src/pages/RecuperarSenha.jsx
+++ lol/src/pages/RecuperarSenha.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Outlet, Link } from "react-router-dom";
 import '../styles/RecuperarSenha.css'
-
+import ImgBackground from "../image/login.png"
+import lux from "../image/lux.png"
 const RecuperarSenha = () => {
   return (
-    <div>
-<li>  <Link to="/">LandingPage</Link></li>
+    <div className='RecuperarSenha'style={{backgroundImage:`url(${ImgBackground})`}} >
+<div className="RSPage-main">
+  
+<li>  <Link to="/">Landingpage</Link></li>
+<li2>  <Link to="/Escrever-se">Escrever-se</Link></li2>
+
+<section>
+  <div className="Recupera-RSPage">
+    <div className="text-RSpage">
+      <h1>Recuperar Senha</h1>
+      <input id='Name' name='Name' placeholder='Name' type="text"  />
     </div>
+  </div>
+</section>
+<section className='Imglux'>
+    <img src={lux} alt="" />
+</section>
+</div>
+</div>
   )
 }
 
