24c7e2de1b8721b5bc30a5cfc4be06fc299d3aa2
Programadores Cariocas 2022.1 <3968082022.1@senacrjedu.local>
2023-02-16 10:23:31 -0300

    commit do nopage

--- lol/src/pages/NotFound.jsx
+++ lol/src/pages/NotFound.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Header from "../components/Header"
 import  Footer from "../components/Footer"
+import akali from "../image/akali.png"
 import "../styles/NotFound.css"
 import "../styles/Global.css"
 
 const NotFound = () => {
   return (
  <div className="notFound">
-   <Header/>
-    
-    <Footer/>
- </div>
+   <Header>
+   </Header>
+   <section className='Imgakali'>
+    <h4>404 - No Found</h4>
+    <h1>Página Não Encontrada</h1>
+    <img src={akali} alt="" />
+   </section> 
+   </div> 
   )
 }
 
