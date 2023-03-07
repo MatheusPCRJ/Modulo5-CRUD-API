e0828f3cc2990e4b7d4f472434cc93f7289fd3b3
Matheus Gomes <matheusoutros@hotmail.com>
2023-03-07 20:24:58 -0300

    HTML pronto para estilização, apaguei o conteúdo do css para começar a estilizar.

--- lol/src/pages/NotFound.jsx
+++ lol/src/pages/NotFound.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react"
 import Header from "../components/Header"
-import  Footer from "../components/Footer"
 import akali from "../image/akali.png"
+import { Link } from "react-router-dom";
 import "../styles/NotFound.css"
 import "../styles/Global.css"
 
 const NotFound = () => {
   return (
- <div className="notFound">
-   <Header>
-   </Header>
-   <section className='Imgakali'>
-    <h4>404 - No Found</h4>
-    <h1>Página Não Encontrada</h1>
-    <img src={akali} alt="" />
-   </section> 
-   </div> 
+    <>
+      <Header/>
+      <main>
+        <h1>404 -Not Found </h1>
+        <p>Pagina nao encontrada</p>
+        <p>
+          Parece que você está perdido soldado <br /> 
+          Clique aqui para achar sua <Link to="/home">Guarnição</Link>
+        </p>
+        <img src={akali}/>
+
+      </main>
+    
+    </>
+ 
   )
 }
 
