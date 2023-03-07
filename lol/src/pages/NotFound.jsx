eb138a6660e89ad7e248f0a04b6d2c3f82cef32e
Matheus Gomes <matheusoutros@hotmail.com>
2023-03-07 20:56:37 -0300

    Página estilização com algumas modificações no HTML, estilizada para celular, faltando Tablet para cima.

--- lol/src/pages/NotFound.jsx
+++ lol/src/pages/NotFound.jsx
@@ -1,28 +1,29 @@
-import React from "react"
-import Header from "../components/Header"
-import akali from "../image/akali.png"
+import React from "react";
+import Header from "../components/Header";
+import akali from "../image/akali.png";
 import { Link } from "react-router-dom";
-import "../styles/NotFound.css"
-import "../styles/Global.css"
+import "../styles/NotFound.css";
+import "../styles/Global.css";
 
 const NotFound = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
-        <h1>404 -Not Found </h1>
-        <p>Pagina nao encontrada</p>
-        <p>
-          Parece que você está perdido soldado <br /> 
-          Clique aqui para achar sua <Link to="/home">Guarnição</Link>
-        </p>
-        <img src={akali}/>
-
+        <div className="div-pai-PCRJ">
+          <div className="div-texto-PCRJ">
+            <h1>404 - Not Found </h1>
+            <p className="traducao-PCRJ">Pagina nao encontrada</p>
+            <p className="mensagem-PCRJ">
+              Parece que você está perdido soldado <br />
+              Clique aqui para achar sua <Link to="/home">Guarnição</Link>
+            </p>
+          </div>
+          <img src={akali} />
+        </div>
       </main>
-    
     </>
- 
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
