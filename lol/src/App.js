c4042430f2d2e0f1af53891f96862bcdae3f6d7f
Matheus Gomes <matheusoutros@hotmail.com>
2023-03-10 12:03:23 -0300

    voltando com o projeto pq o merge deu ruim

---
+++ lol/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1> ricardo  aqui</h1>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ lol/src/pages/Skins.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/Global.css"
+import "../styles/Skins.css"
+import Header from "../components/Header"
+import  Footer from "../components/Footer"
+
+const Skins = () => {
+  return (
+    <div className="skins">
+       <Header/>
+    
+    <Footer/>
+    </div>
+  )
+}
+
+export default Skins
