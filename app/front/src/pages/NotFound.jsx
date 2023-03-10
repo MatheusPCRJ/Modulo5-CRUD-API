46a3b6211fe203a191ba3058402c273d67190ba7
Matheus Gomes <112782424+MatheusPCRJ@users.noreply.github.com>
2023-03-10 12:41:09 -0300

    Atualizando o Projeto!

--- app/front/src/pages/NotFound.jsx
+++ app/front/src/pages/NotFound.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Header from "../components/Header"
-import  Footer from "../components/Footer"
-import "../styles/NotFound.css"
-import "../styles/Global.css"
+import React from "react";
+import Header from "../components/Header";
+import akali from "../image/akali.png";
+import { Link } from "react-router-dom";
+import "../styles/NotFound.css";
+import "../styles/Global.css";
 
 const NotFound = () => {
   return (
- <div className="notFound">
-   <Header/>
-    
-    <Footer/>
- </div>
-  )
-}
+    <>
+      <Header />
+      <main>
+        <div className="div-pai-PCRJ">
+          <div className="div-texto-PCRJ">
+            <h1>404 - Not Found </h1>
+            <p className="traducao-PCRJ">Pagina nao encontrada</p>
+            <p className="mensagem-PCRJ">
+              Parece que você está perdido soldado. <br />
+              Clique aqui para achar sua <Link to="/home">Guarnição</Link>!
+            </p>
+          </div>
+          <img src={akali} />
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
--- app/front/src/pages/Skins.jsx
+++ app/front/src/pages/Skins.jsx
@@ -3,13 +3,12 @@ import "../styles/Global.css"
 import "../styles/Skins.css"
 import Header from "../components/Header"
 import  Footer from "../components/Footer"
-import CardSkins from "../components/CardSkins"
 
 const Skins = () => {
   return (
     <div className="skins">
        <Header/>
-    <CardSkins/>
+    
     <Footer/>
     </div>
   )
